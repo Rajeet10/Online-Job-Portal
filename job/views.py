3fd0e8388bff15a4ea6c16b2d276b3dc0eb44826
Rajeet <rajeetchaudhary10@gmail.com>
2021-12-16 00:09:48 +0530

    adding usersignup validationa nd send to database

--- job/views.py
+++ job/views.py
@@ -1,5 +1,7 @@
+from os import error
 from django.shortcuts import render
-
+from django.contrib.auth.models import User
+from .models import *
 # Create your views here.
 
 def index(request):
@@ -15,4 +17,20 @@ def recruiter_login(request):
     return render(request,'recruiter_login.html')
 
 def user_signup(request):
-    return render(request,'user_signup.html')
+    error=""
+    if request.method=="POST":
+        f=request.POST['fname']
+        l=request.POST['lname']
+        i=request.FILES['image']
+        p=request.POST['pwd']
+        e=request.POST['email']
+        con=request.POST['contact']
+        gen=request.POST['gender']
+        try:
+            user=User.objects.create_user(first_name=f,last_name=l,username=e,password=p)
+            StudentUser.objects.create(user=user,mobile=con,image=i,gender=gen,type="student")
+            error="no"
+        except:
+            error="yes"
+    d={'error':error}
+    return render(request,'user_signup.html',d)
