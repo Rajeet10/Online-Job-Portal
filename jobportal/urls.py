45e09c1378c35db30207c0a7197eb882c06895dd
Rajeet <rajeetchaudhary10@gmail.com>
2021-12-15 23:46:00 +0530

    Added media folder to store photo

--- jobportal/urls.py
+++ jobportal/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from job.views import *
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -26,4 +28,4 @@ urlpatterns = [
     path('user_signup',user_signup,name="user_signup")
 
 
-]
+]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
